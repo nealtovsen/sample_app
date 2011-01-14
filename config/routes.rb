6171d25a6709caaa3044af266307e15a5b46ad19
Neal Tovsen <neal@telemetryweb.com>
2011-01-14 16:44:15 -0600

    html helpers

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ SampleApp::Application.routes.draw do
   get "pages/home"
   get "pages/contact"
   get "pages/about"
+  get "pages/help"
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App" 
+  end
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -11,9 +15,9 @@ describe PagesController do
     
     it "should have the right title" do
       get 'home'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Home")
+      response.should have_selector("title",
+                                    :content => @base_title + " | Home")
     end
-
   end
 
   describe "GET 'contact'" do
@@ -21,24 +25,24 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
-    
+
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Contact")
+      response.should have_selector("title",
+                                    :content => @base_title + " | Contact")
     end
   end
-  
+
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
     end
-    
+
     it "should have the right title" do
       get 'about'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | About")
+      response.should have_selector("title",
+                                    :content => @base_title + " | About")
     end
   end
-
-
 end
