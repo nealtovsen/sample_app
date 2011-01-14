6cccfd74e52c7281a70affda5606cbe219c5714f
Neal Tovsen <neal@telemetryweb.com>
2011-01-14 16:18:32 -0600

    static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 SampleApp::Application.routes.draw do
   get "pages/home"
-
   get "pages/contact"
+  get "pages/about"
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
