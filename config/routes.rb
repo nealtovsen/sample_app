405e5856ef0db94aec98aadbe1929089ac3ea6f1
Neal Tovsen <neal@telemetryweb.com>
2011-01-20 12:25:42 -0600

    Finished laout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
-  get "pages/help"
+    get "users/new"
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+    root              :to => "pages#home"
+    match '/contact', :to => 'pages#contact'
+    match '/about',   :to => 'pages#about'
+    match '/help',    :to => 'pages#help'
+    match '/signup',  :to => 'users#new'
+
+# You can have the root of your site routed with "root"
+# just remember to delete public/index.html.
+# root :to => "welcome#index"
 
 end
