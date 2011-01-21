c5d12f493bb8bcc5f95a2b6a5c0c88f82ce863f4
Neal Tovsen <neal@telemetryweb.com>
2011-01-20 20:44:49 -0600

    Done with sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 SampleApp::Application.routes.draw do
     resources :users
+    resources :sessions, :only => [:new, :create, :destroy]
 
     root              :to => "pages#home"
     match '/contact', :to => 'pages#contact'
     match '/about',   :to => 'pages#about'
     match '/help',    :to => 'pages#help'
     match '/signup',  :to => 'users#new'
-
-# You can have the root of your site routed with "root"
-# just remember to delete public/index.html.
-# root :to => "welcome#index"
+    match '/signin',  :to => 'sessions#new'
+    match '/signout', :to => 'sessions#destroy'
 
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -91,12 +91,17 @@ describe UsersController do
       it "should redirect to the user show page" do
         post :create, :user => @attr
         response.should redirect_to(user_path(assigns(:user)))
-      end    
+      end
+      
       it "should have a welcome message" do
         post :create, :user => @attr
         flash[:success].should =~ /welcome to the sample app/i
       end
-
+      
+      it "should sign the user in" do
+        post :create, :user => @attr
+        controller.should be_signed_in
+      end
     end
 
   end
